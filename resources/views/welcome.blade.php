4cbf1311096272e39555fe2c4596d21ae52ec3fa
Nguyen Thuy Uyen <nt.uyen3107@gmail.com>
2023-12-05 21:16:19 +0700

    Update detail.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,10 @@
 									<span class="image">
 										<img src="{{ Storage::url('public/' . $product->image) }}" alt="" />
 									</span>
-									<a href="#">
+									<a href="#" data-toggle='modal' data-target='#GuestDetail' 
+										imgURL="{{Storage::url('public/' . $product->image)}}" 
+										namePro="{{$product->name}}"
+									>
 										<h2>{{$product->name}}</h2>
 										
 										<p><strong>{{$product->price}}</strong></p>
@@ -23,9 +26,17 @@
 	@endforeach
 
 	{{ $products->render()}}
-
+@include('guest.detail')
 </section>
 
 
 							
+@endsection
+
+@section('script')
+	var name = $(this).data('namePro')
+
+	$('#GuestDetail').find('.modal-body')
+
+	$('#GuestDetail').modal('show')
 @endsection
