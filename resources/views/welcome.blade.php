46f7c790559b8da2d360bc6ec404e2014172c7d1
Nguyen Thuy Uyen <nt.uyen3107@gmail.com>
2023-12-05 18:41:44 +0700

    Update menu.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,8 @@
 
 <!-- Vacations -->
 <section class="tiles">
-	<div >
-
 	@foreach ($products as $product)
-								<div class="productActive">
+								<article class="style3">
 									<span class="image">
 										<img src="{{ Storage::url('public/' . $product->image) }}" alt="" />
 									</span>
@@ -21,86 +19,12 @@
 											{{$product->description}}
 		                                </p>
 									</a>
-								</div>
+								</article>
 	@endforeach
-	</div>
 
 	{{ $products->render()}}
-<!-- 								
-								<article class="style2">
-									<span class="image">
-										<img src="{{('images/product-2-720x480.jpg') }}" alt="" />
-									</span>
-									<a href="#">
-										<h2>Lorem ipsum dolor sit amet, consectetur</h2>
-										
-										<p><del>$11.00</del> <strong> $9.95</strong></p>
 
-										<p>
-											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, ea.
-		                                </p>
-									</a>
-								</article>
-								<article class="style3">
-									<span class="image">
-										<img src="{{('images/product-3-720x480.jpg') }}" alt="" />
-									</span>
-									<a href="#">
-										<h2>Lorem ipsum dolor sit amet, consectetur</h2>
-										
-										<p><del>$11.00</del> <strong> $9.95</strong></p>
-
-										<p>
-											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, qui.
-		                                </p>
-									</a>
-								</article>
-
-								<article class="style4">
-									<span class="image">
-										<img src="{{('images/product-4-720x480.jpg') }}" alt="" />
-									</span>
-									<a href="#">
-										<h2>Lorem ipsum dolor sit amet, consectetur</h2>
-										
-										<p><del>$11.00</del> <strong> $9.95</strong></p>
-
-										<p>
-											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, non!
-		                                </p>
-									</a>
-								</article>
-
-								<article class="style5">
-									<span class="image">
-										<img src="{{('images/product-5-720x480.jpg') }}" alt="" />
-									</span>
-									<a href="#">
-										<h2>Lorem ipsum dolor sit amet, consectetur</h2>
-										
-										<p><del>$11.00</del> <strong> $9.95</strong></p>
-
-										<p>
-											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum, nam.
-		                                </p>
-									</a>
-								</article>
-
-								<article class="style6">
-									<span class="image">
-										<img src="{{('images/product-6-720x480.jpg') }}" alt="" />
-									</span>
-									<a href="#">
-										<h2>Lorem ipsum dolor sit amet, consectetur</h2>
-										
-										<p><del>$11.00</del> <strong> $9.95</strong></p>
-
-										<p>
-											Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, quod.
-		                                </p>
-									</a>
-								</article> -->
-							</section>
+</section>
 
 
 							
